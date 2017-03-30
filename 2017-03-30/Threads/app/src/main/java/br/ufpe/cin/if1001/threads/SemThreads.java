18f2e954eab1019899fceb5a4a3ead1918d8fbc7
Leopoldo Teixeira <leopoldomt@gmail.com>
2017-03-30 20:19:46 -0300

    codigo da aula de 30.03.2017, introduzindo o problema de chamar operacoes de rede na main thread, e diversos cenarios de setup para realizar operacoes de background em apps

---
+++ 2017-03-30/Threads/app/src/main/java/br/ufpe/cin/if1001/threads/SemThreads.java
@@ -0,0 +1,59 @@
+package br.ufpe.cin.if1001.threads;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SemThreads extends Activity {
+    private Bitmap mBitmap;
+    private ImageView mIView;
+    private int mDelay = 25000;
+    int toasts = 0;
+    private TextView contadorToasts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.threads);
+        mIView = (ImageView) findViewById(R.id.imageView);
+        contadorToasts = (TextView) findViewById(R.id.contadorToasts);
+
+        final Button loadButton = (Button) findViewById(R.id.loadButton);
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadIcon();
+                if (null != mBitmap) {
+                    mIView.setImageBitmap(mBitmap);
+                }
+            }
+        });
+
+        final Button otherButton = (Button) findViewById(R.id.otherButton);
+        otherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toasts++;
+                contadorToasts.setText(getString(R.string.contador_de_toasts) + toasts);
+                Toast.makeText(getApplicationContext(), "Estou trabalhando... (" + toasts + ")", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loadIcon() {
+        try {
+            Thread.sleep(mDelay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.painter);
+    }
+
+
+}
