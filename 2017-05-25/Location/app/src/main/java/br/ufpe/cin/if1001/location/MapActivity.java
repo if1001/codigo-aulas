a661362fbe37f9537b7eea872c353a53e9ebded3
Leopoldo Teixeira <leopoldomt@gmail.com>
2017-05-25 15:17:10 -0300

    location and telephony projects - commit inicia

---
+++ 2017-05-25/Location/app/src/main/java/br/ufpe/cin/if1001/location/MapActivity.java
@@ -0,0 +1,53 @@
+package br.ufpe.cin.if1001.location;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    public final static String LAT_KEY = "latitude";
+    public final static String LON_KEY = "longitude";
+
+    private double latitude;
+    private double longitude;
+
+    private GoogleMap gMap;
+    //nao estou checando que tem acesso ao play services como no caso de FusedLocation...
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        Intent i = getIntent();
+        latitude  = i.getDoubleExtra(LAT_KEY,0);
+        longitude = i.getDoubleExtra(LON_KEY,0);
+
+        SupportMapFragment mapa = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa);
+        mapa.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+        LatLng location = new LatLng(latitude, longitude);
+        gMap.addMarker(new MarkerOptions().position(location).title("Marcador!"));
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+        gMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+
+
+    }
+}
